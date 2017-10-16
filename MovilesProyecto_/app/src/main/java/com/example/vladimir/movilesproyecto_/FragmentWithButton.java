cb648e14b0b57dfddcc2b3efad30c493b6719223
Vlad04 <vladiir.rod96@gmail.com>
2017-10-15 21:01:57 -0500

    second commit

---
+++ MovilesProyecto_/app/src/main/java/com/example/vladimir/movilesproyecto_/FragmentWithButton.java
@@ -0,0 +1,138 @@
+package com.example.vladimir.movilesproyecto_;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.vladimir.movilesproyecto_.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FragmentWithButton.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FragmentWithButton#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+
+
+public class FragmentWithButton extends Fragment implements ValueEventListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    ArrayList<String> categorias;
+    //CategoriaAdapter categoriasAdapter;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference Root_reference = firebaseDatabase.getReference();
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fragment_with_button, container, false);
+
+        return view;
+    }
+
+
+
+    public FragmentWithButton() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentWithButton.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentWithButton newInstance(String param1, String param2) {
+        FragmentWithButton fragment = new FragmentWithButton();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void toastText(String text, String text2);
+    }
+}
--- MovilesProyecto_/app/src/main/java/com/example/vladimir/movilesproyecto_/MainActivity.java
+++ MovilesProyecto_/app/src/main/java/com/example/vladimir/movilesproyecto_/MainActivity.java
@@ -1,25 +1,62 @@
 package com.example.vladimir.movilesproyecto_;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentWithButton.OnFragmentInteractionListener{
 
-    private Button back_btn;
+    private Button sign_out;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        back_btn=(Button)findViewById(R.id.back_btn);
-        back_btn.setOnClickListener(new View.OnClickListener() {
+
+        sign_out=(Button)findViewById(R.id.Sign_out_button);
+        sign_out.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,LogInActivity.class);
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, LogInActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    public void Create_fragment(View v)
+    {
+        FragmentManager manager=getFragmentManager();
+        FragmentTransaction transaction=manager.beginTransaction();
+        FragmentWithButton newFragment=FragmentWithButton.newInstance("Hello","Vlad");
+        transaction.add(R.id.fragment_space,newFragment, "button");
+        Toast.makeText(getApplicationContext(), "Por favor seleccione un tipo de recarga", Toast.LENGTH_SHORT).show();
+        transaction.commit();
+    }
+/*
+    public void remove_fragment(View v)
+    {
+        FragmentManager manager=getFragmentManager();
+        FragmentTransaction transaction=manager.beginTransaction();
+        FragmentWithButton oldFragment=(FragmentWithButton)manager.findFragmentByTag("button");
+        if(oldFragment!=null)
+        {
+            transaction.remove(oldFragment);
+            transaction.commit();
+        }
+    }
+*/
+
+
+
+
+    @Override
+    public void toastText(String text, String text2) {
+
     }
 }
